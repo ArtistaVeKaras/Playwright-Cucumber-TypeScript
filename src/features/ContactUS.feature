cf566ae64da276d5888a81cdd21e514680658307
Claudio.Correia <claudiooartista@hotmail.co.uk>
2025-11-28 16:00:44 +0000

    Created a new feature step-definition files

---
+++ src/features/ContactUS.feature
@@ -0,0 +1,11 @@
+Feature: WebdriverUniversity.com - Contact us Page
+
+    Scenario: Valid Contact Us Form submis
+    Given I navigate to webdriveruniversity hompepage
+    When I click on the Contact Us button
+    And I type a valid first name
+    And I type a valid last name
+    And I type a valid email address
+    And I type a comment into the comment text area
+    And I click on the submit button
+    Then I should be presented with a successful contact us submission message
---
+++ src/step-definitions/Homepage_Steps.ts
@@ -0,0 +1,44 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('I navigate to webdriveruniversity hompepage', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+
+When('I click on the Contact Us button', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+
+When('I type a valid first name', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+
+When('I type a valid last name', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+When('I type a valid email address', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+When('I type a comment into the comment text area', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+When('I click on the submit button', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
+
+Then('I should be presented with a successful contact us submission message', function () {
+    // Write code here that turns the phrase above into concrete actions  
+    return 'pending';
+});
