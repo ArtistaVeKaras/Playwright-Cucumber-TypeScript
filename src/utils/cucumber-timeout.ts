151be3fe8d12926915c94be539e67e2b7326be7a
Claudio.Correia <claudiooartista@hotmail.co.uk>
2025-12-15 09:34:38 +0000

    Implemented Centralized default timeout

--- src/utils/cucumber-timeout.ts
+++ src/utils/cucumber-timeout.ts
@@ -1,5 +1,9 @@
 import { setDefaultTimeout } from "@cucumber/cucumber";
+import dotenv from 'dotenv';
+dotenv.config({ path : './env/.env' });
 
 // If too low this will affect playwright timeouts
 // Example exception: Error: functions timed out, ensure the promise resolves within 20000 miliseconds
-setDefaultTimeout(60000);
+
+const cucumberStepTimeout = parseInt(process.env.CUCUMBER_STEP_TIMEOUT || '60000'); // Default to 60 seconds
+setDefaultTimeout(cucumberStepTimeout);
--- src/utils/playwright-timeouts.ts
+++ src/utils/playwright-timeouts.ts
@@ -1,9 +1,15 @@
 import { Page } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config({ path : './env/.env' });
 
 export function setGlobalTimeouts(page: Page) {
+
+    const navigationTimeout = parseInt(process.env.UI_AUTOMATION_NAVIGATION_TIMEOUT || '50000'); // Default to 50 seconds
+    const commandTimeout = parseInt(process.env.UI_AUTOMATION_COMMAND_TIMEOUT || '30000'); // Default to 30 seconds
+
     // Set Global 'navigationTimeout' and 'timeout' for the page
-    page.setDefaultNavigationTimeout(50000); // 50 seconds for navigation timeout
+    page.setDefaultNavigationTimeout(navigationTimeout); // 50 seconds for navigation timeout
 
     // Set default timeout for all actions (click, fill, etc.)
-    page.setDefaultTimeout(30000); // 30 seconds for other actions
+    page.setDefaultTimeout(commandTimeout); // 30 seconds for other actions
 }
